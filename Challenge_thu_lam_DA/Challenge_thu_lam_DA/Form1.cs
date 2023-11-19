3a8ee6dac93927658a4ba34966d1158604eb0863
Ho Pham Phu An <22520013@gm.uit.edu.vn>
2023-11-19 22:44:37 +0700

    Nop lan 1

---
+++ Challenge_thu_lam_DA/Challenge_thu_lam_DA/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Challenge_thu_lam_DA
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+            bunifuFormDock1.SubscribeControlToDragEvents(bunifuGradientPanel1);
+            bunifuFormDock1.SubscribeControlToDragEvents(tabPage1);
+            bunifuFormDock1.SubscribeControlToDragEvents(tabPage2);
+
+        }
+        private void Form_Login_Load(object sender, EventArgs e)
+        {
+            ReFresh1();// gọi hàm sửa lỗi
+        }
+        public void ReFresh1()
+        {
+            tbx_username.Refresh();
+            tbx_password.Refresh();
+            tbx_email.Refresh();
+        }
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_signin_Click(object sender, EventArgs e)
+        {
+            bunifuPages1.SetPage(0);
+         
+        }
+
+        private void btn_signup_Click(object sender, EventArgs e)
+        {
+            bunifuPages1.SetPage(1);
+            
+        }
+
+    }
+}
---
+++ Challenge_thu_lam_DA/Challenge_thu_lam_DA/Modify/Modify.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_thu_lam_DA.Modify
+{
+    internal class Modify
+    {
+       
+    }
+}
